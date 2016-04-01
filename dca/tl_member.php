ae2ca34438a9677d8c064530418fb0a216648893
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-04-01 17:14:19 +0200

    refactored date added and alias

--- dca/tl_member.php
+++ dca/tl_member.php
@@ -2,27 +2,10 @@
 
 $arrDca = &$GLOBALS['TL_DCA']['tl_member'];
 
-/**
- * Palettes
- */
-$arrDca['palettes']['default'] = str_replace('lastname,', 'lastname,alias,', $arrDca['palettes']['default']);
+\HeimrichHannot\Haste\Dca\General::addAliasToDca('tl_member',
+		array('tl_member_member_content_archives', 'generateAlias'), 'lastname');
 
-/**
- * Fields
- */
-$arrDca['fields']['alias'] = array
-(
-	'label'     => &$GLOBALS['TL_LANG']['tl_member']['alias'],
-	'exclude'   => true,
-	'search'    => true,
-	'inputType' => 'text',
-	'eval'      => array('rgxp' => 'alias', 'unique' => true, 'maxlength' => 128, 'tl_class' => 'w50'),
-	'save_callback' => array
-	(
-		array('tl_member_member_content_archives', 'generateAlias')
-	),
-	'sql' => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
-);
+\HeimrichHannot\Haste\Dca\General::addAliasButton('tl_member');
 
 class tl_member_member_content_archives extends \Backend {
 
--- languages/de/tl_member.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$arrLang = &$GLOBALS['TL_LANG']['tl_member'];
-
-/**
- * Fields
- */
-$arrLang['alias'] = array('Alias', 'Der Alias ist eine eindeutige Referenz, die anstelle der numerischen ID aufgerufen werden kann.');
