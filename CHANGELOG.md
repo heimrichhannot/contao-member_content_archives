fcc6184555d26c0d9b9a2eba4bb5faec3bc96277
Oliver Janke <o.janke@heimrich-hannot.de>
2016-12-20 16:02:43 +0100

    see changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+## [1.0.20] - 2016-12-20
+
+### Added
+- CHANGELOG.md
+- findPublishedByMid() to MemberContentArchiveModel
--- models/MemberContentArchiveModel.php
+++ models/MemberContentArchiveModel.php
@@ -4,7 +4,21 @@ namespace HeimrichHannot\MemberContentArchives;
 
 class MemberContentArchiveModel extends \Model
 {
+    protected static $strTable = 'tl_member_content_archive';
 
-	protected static $strTable = 'tl_member_content_archive';
+    public static function findPublishedByMid($intMid, array $arrOptions = array())
+    {
+        $t = static::$strTable;
 
+        $arrColumns = array("$t.mid=?");
+        $arrValues  = array($intMid);
+
+        if (!BE_USER_LOGGED_IN)
+        {
+            $arrColumns[] = "$t.published=?";
+            $arrValues[]  = 1;
+        }
+
+        return static::findBy($arrColumns, $arrValues, $arrOptions);
+    }
 }
